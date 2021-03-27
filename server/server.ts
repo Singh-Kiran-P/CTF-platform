07e947e9b6ba6e544fe61e45a8427713dab75772
Singh Kiran <kiran.singh@student.uhasselt.be>
2021-03-27 13:03:55 +0100

    code base

---
+++ server/server.ts
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+
+app.get('/', (req,res) => {
+  res.send('hello from expsdfredss kirassssz');
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port,() => console.log(`App listening on port ${port}`));
