4b7837b3b54db287a9e44d94c2cca27ffaaa4543
kiran <kiran.singh@student.uhasselt.be>
2021-04-02 12:03:19 +0200

    working on docker scripts

--- Docker/Node-docker/Docker_scripts/make-start-challenge-container.js
+++
@@ -1,6 +0,0 @@
-var Docker = require('dockerode');
-var docker = new Docker({ socketPath: '/var/run/docker.sock' });
-docker.createContainer({ Image: 'challenge1', name: 'team2-challenge2', HostConfig: { PortBindings: { '8080/tcp': [{ HostPort: '8081' }] } } }, function (err, container) {
-  container.start(function (err, data) {
-  });
-});
