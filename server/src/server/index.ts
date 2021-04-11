19cdb9b88778387be140ced671aa512082ae5a5c
Singh Kiran <kiran.singh@student.uhasselt.be>
2021-04-11 11:56:59 +0200

    Added express router [ Template code ]

--- server/src/server/index.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-import express from 'express';
-dotenv.config();
-
-// TODO: add more functionality to server? custom Server class?
-
-const app = express();
-app.listen(process.env.SERVER_PORT);
-export default app;
