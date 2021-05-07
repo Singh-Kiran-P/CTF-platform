6b69af37c0f910ef9760969191e7113c34371b33
kiran <kiran.singh@student.uhasselt.be>
2021-05-08 00:06:36 +0200

    notifications refactored [server]

--- server/src/controllers/notification.ts
+++ server/src/controllers/notification.ts
@@ -1,44 +1,74 @@
+import { Request, Response, NextFunction } from 'express';
 import DB, { Notification } from '../database';
 import socketIO from './socket';
 
-async function send(data: any) {
-    let title: string = data.title;
-    let msg: string = data.msg;
-
-    //save to db
-    let notificationRepo = DB.repo(Notification);
-    let notification = new Notification(title, msg);
-    await notificationRepo.save(notification);
-
-    // emit socket
-
-    let socket = socketIO.getIO();
-    socket.emit('notification', data);
-    console.log(data);
-
-}
-
-function getAllNotifications() {
-    let notificationRepo = DB.repo(Notification);
-    return notificationRepo.find({});
-}
-
-function deleteById(_id: number) {
+function getAllNotifications_GET(req: Request, res: Response) {
     let notificationRepo = DB.repo(Notification);
     notificationRepo
-    return notificationRepo.delete({ id: _id });
+        .find()
+        .then((data) => {
+            res.json(data);
+        })
+        .catch(() => {
+            res.send('error');
+        });
+}
+
+function send_POST(req: Request, res: Response) {
+    let data = req.fields;
+
+    let title: string = data.title.toString();
+    let msg: string = data.msg.toString();
+
+    new Promise<void>(async (resolve, reject) => {
+        //save to db
+        let notificationRepo = DB.repo(Notification);
+        let notification = new Notification(title, msg);
+        await notificationRepo.save(notification);
+
+        // emit socket
+        let socket = socketIO.getIO();
+        socket.emit('notification', data);
+        console.log(data);
+    })
+        .then((data) => {
+            res.json({ message: 'Notification send successfully!', statusCode: 205 });
+        })
+        .catch(() => {
+            res.send('error');
+        })
 
 }
 
-async function deleteAll() {
-    await DB.conn.query('DELETE FROM Notification;');
+function deleteById_DELETE(req: Request, res: Response) {
+    let _id: number = Number(req.fields.id.toString());
+    console.log(_id);
+    let notificationRepo = DB.repo(Notification);
+
+    notificationRepo
+        .delete({ id: _id })
+        .then(() => {
+            res.json({ message: 'Notification deleted successfully!', statusCode: 200 });
+        })
+        .catch((err) => {
+            res.json({ message: err, statusCode: 404 });
+        })
+
 }
 
+function deleteAll_DELETE(req: Request, res: Response) {
+    DB.conn.query('DELETE FROM Notification;').then(() => {
+        res.json({ message: 'All notifications deleted successfully!', statusCode: 200 });
+    })
+    .catch((err) => {
+        res.json({ message: err, statusCode: 404 });
+    })
+}
 
 export default {
-    send,
-    getAllNotifications,
-    deleteById,
-    deleteAll
+    getAllNotifications_GET,
+    send_POST,
+    deleteById_DELETE,
+    deleteAll_DELETE
 }
 
--- server/src/routes/notification.ts
+++ server/src/routes/notification.ts
@@ -3,47 +3,12 @@ import express from "express";
 const router = express.Router();
 import notificationController from "../controllers/notification";
 
-router.get('/getAll', (req, res) => {
-    notificationController.getAllNotifications()
-        .then((data) => {
-            res.json(data);
-        })
-        .catch(() => {
-            res.send('error');
-        })
-});
+router.get('/getAll', notificationController.getAllNotifications_GET);
 
-router.post('/send', (req, res) => {
-    let data = req.fields;
-    notificationController.send(data)
-        .then((data) => {
-            res.json({ message: 'Notification send successfully!', statusCode: 205 });
-        })
-        .catch(() => {
-            res.send('error');
-        })
-});
+router.post('/send', notificationController.send_POST);
 
-router.delete('/delete', isAdmin, (req, res) => {
-    let id: number = Number(req.fields.id.toString());
-    console.log(id);
+router.delete('/deleteById', notificationController.deleteById_DELETE);
 
-    notificationController.deleteById(id)
-        .then(() => {
-            res.json({ message: 'Notification deleted successfully!', statusCode: 200 });
-        })
-        .catch((err) => {
-            res.json({ message: err, statusCode: 404 });
-        })
-});
-
-router.delete('/deleteAll',  (req, res) => {
-    notificationController.deleteAll().then(() => {
-        res.json({ message: 'All notifications deleted successfully!', statusCode: 200 });
-    })
-        .catch((err) => {
-            res.json({ message: err, statusCode: 404 });
-        })
-});
+router.delete('/deleteAll', notificationController.deleteAll_DELETE);
 
 export default { path: '/notification', router };
