ec85201243bee0397e465c8df52d7d6184a82265
kiran <kiran.singh@student.uhasselt.be>
2021-05-20 10:49:00 +0200

    chart typescript

--- client/src/pages/Leaderboard.vue
+++ client/src/pages/Leaderboard.vue
@@ -1,19 +1,73 @@
 <template>
-  <div class="leaderboard">
-    <span>This is the leaderboard</span>
-  </div>
+    <div class="hello" ref="chartdiv"></div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+import { any } from '@amcharts/amcharts4/.internal/core/utils/Array';
 
-export default Vue.extend({
-    name: 'Leaderboard',
-});
+am4core.useTheme(am4themes_animated);
+
+export default {
+    name: "HelloWorld",
+    data: () => ({
+        chart: any,
+    }),
+    mounted() {
+        this.chart = am4core.create(this.$refs.chartdiv, am4charts.XYChart);
+
+        chart.paddingRight = 20;
+
+        let data = [];
+        let visits = 10;
+        for (let i = 1; i < 366; i++) {
+            visits += Math.round(
+                (Math.random() < 0.5 ? 1 : -1) * Math.random() * 10
+            );
+            data.push({
+                date: new Date(2018, 0, i),
+                name: "name" + i,
+                value: visits,
+            });
+        }
+
+        chart.data = data;
+
+        let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+        dateAxis.renderer.grid.template.location = 0;
+
+        let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+        valueAxis.tooltip.disabled = true;
+        valueAxis.renderer.minWidth = 35;
+
+        let series = chart.series.push(new am4charts.LineSeries());
+        series.dataFields.dateX = "date";
+        series.dataFields.valueY = "value";
+
+        series.tooltipText = "{valueY.value}";
+        chart.cursor = new am4charts.XYCursor();
+
+        let scrollbarX = new am4charts.XYChartScrollbar();
+        scrollbarX.series.push(series);
+        chart.scrollbarX = scrollbarX;
+
+        this.chart = chart;
+    },
+
+    beforeDestroy() {
+        if (this.chart) {
+            this.chart.dispose();
+        }
+    },
+};
 </script>
 
-<style scoped lang="scss">
-span {
-  font-size: 2rem;
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+    width: 100%;
+    height: 500px;
 }
 </style>
