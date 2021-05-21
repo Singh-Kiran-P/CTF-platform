0d78fbaa89e8c5a910971aac6a71345dadeece88
Senn <senn.robyns@student.uhasselt.be>
2021-05-21 11:58:28 +0200

    Further frontend work

--- client/src/pages/Leaderboard.vue
+++ client/src/pages/Leaderboard.vue
@@ -1,5 +1,19 @@
 <template>
-    <div class="hello" id="chartdiv"></div>
+    <div class="leaderboard">
+        <div class="chart-settings-div">
+            <b-form-group>
+                <label for="time-span-select">Time Span</label>
+                <b-form-select 
+                    @change="handleTimeSpanSelect"
+                    size=sm
+                    id="time-span-select"
+                    v-model="timeSpan"
+                    :options="timeSpanOptions"
+                ></b-form-select>
+            </b-form-group>
+        </div>
+        <div class="chart-div" id="chart"></div>
+    </div>
 </template>
 
 <script lang="ts">
@@ -15,77 +29,12 @@ export default Vue.extend({
     name: "Leaderboard",
     components: {},
     data: () => ({
-        chart: null as unknown as am4charts.XYChart //TODO: fix this
+        chart: null as unknown as am4charts.XYChart,
+        timeSpan: "Weekly",
+        timeSpanOptions: ["Hourly", "Daily", "Weekly"]
     }),
     created() {},
     methods: {
-        createAxisAndSeries(field: string, name: string, opposite: boolean, bullet: string) {
-            var valueAxis = this.chart.yAxes.push(new am4charts.ValueAxis());
-            if (this.chart.yAxes.indexOf(valueAxis) != 0) {
-                valueAxis.syncWithAxis = <am4charts.ValueAxis>(
-                    this.chart.yAxes.getIndex(0)
-                );
-            }
-
-            var series = this.chart.series.push(new am4charts.StepLineSeries());
-            series.dataFields.valueY = field;
-            series.dataFields.dateX = "date";
-            series.strokeWidth = 2;
-            series.yAxis = valueAxis;
-            series.name = name;
-            series.tooltipText = "{name}: [bold]{valueY}[/]";
-            series.tensionX = 0.8;
-            series.showOnInit = true;
-
-            var interfaceColors = new am4core.InterfaceColorSet();
-
-            switch (bullet) {
-                case "triangle":
-                    var newBullet = series.bullets.push(
-                        new am4charts.Bullet()
-                    );
-                    newBullet.width = 12;
-                    newBullet.height = 12;
-                    newBullet.horizontalCenter = "middle";
-                    newBullet.verticalCenter = "middle";
-
-                    var triangle = newBullet.createChild(am4core.Triangle);
-                    triangle.stroke = interfaceColors.getFor("background");
-                    triangle.strokeWidth = 2;
-                    triangle.direction = "top";
-                    triangle.width = 12;
-                    triangle.height = 12;
-                    break;
-                case "rectangle":
-                    var newBullet = series.bullets.push(
-                        new am4charts.Bullet()
-                    );
-                    newBullet.width = 10;
-                    newBullet.height = 10;
-                    newBullet.horizontalCenter = "middle";
-                    newBullet.verticalCenter = "middle";
-
-                    var rectangle = newBullet.createChild(am4core.Rectangle);
-                    rectangle.stroke = interfaceColors.getFor("background");
-                    rectangle.strokeWidth = 2;
-                    rectangle.width = 10;
-                    rectangle.height = 10;
-                    break;
-                default:
-                    var defaultBullet = series.bullets.push(
-                        new am4charts.CircleBullet()
-                    );
-                    defaultBullet.circle.stroke = interfaceColors.getFor("background");
-                    defaultBullet.circle.strokeWidth = 2;
-                    break;
-            }
-
-            valueAxis.renderer.line.strokeOpacity = 1;
-            valueAxis.renderer.line.strokeWidth = 2;
-            valueAxis.renderer.line.stroke = series.stroke;
-            valueAxis.renderer.labels.template.fill = series.stroke;
-            valueAxis.renderer.opposite = opposite;
-        },
         createSeries(field: string, name: string, valueAxis: am4charts.ValueAxis<am4charts.AxisRenderer>) {
             var series = this.chart.series.push(new am4charts.StepLineSeries());
             series.dataFields.valueY = field;
@@ -100,7 +49,6 @@ export default Vue.extend({
             var defaultBullet = series.bullets.push(new am4charts.CircleBullet());
             defaultBullet.circle.stroke = interfaceColors.getFor("background");
             defaultBullet.circle.strokeWidth = 2;
-
         },
         generateChartData() {
             var chartData = [];
@@ -120,7 +68,7 @@ export default Vue.extend({
                 newDate.setDate(newDate.getDate() + i);
 
                 team1_points += (i + 5) * 10;
-                team2_points += (i + 120) * 10;
+                team2_points += (i + 23) * 10;
                 team3_points += i * 10;
 
                 chartData.push({ // Save date and points per team on this date
@@ -132,9 +80,12 @@ export default Vue.extend({
             }
             return chartData;
         },
+        handleTimeSpanSelect(span: string) {
+            console.log(span);
+        }
     },
     mounted() {
-        this.chart = am4core.create("chartdiv", am4charts.XYChart);  
+        this.chart = am4core.create("chart", am4charts.XYChart);  
         this.chart.colors.step = 2;// Increase contrast by taking every second color
 
         // Add data
@@ -158,7 +109,7 @@ export default Vue.extend({
         // Add cursor
         this.chart.cursor = new am4charts.XYCursor();
 
-        // generate some random data, quite different range
+        // generate scrollbar
         let scrollbarX = new am4charts.XYChartScrollbar();
         this.chart.scrollbarX = scrollbarX;
     },
@@ -178,8 +129,10 @@ body {
         Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
         "Segoe UI Symbol";
 }
-
-#chartdiv {
+.chart-settings{
+    margin-top: var(--margin);
+}
+.chart-div {
     width: 100%;
     height: 500px;
 }
