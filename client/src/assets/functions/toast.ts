62cbe967f3c452af78f690cf3a96a5a7c587b81e
kiran <kiran.singh@student.uhasselt.be>
2021-05-30 12:08:36 +0200

    toast

--- client/src/assets/functions/toast.ts
+++ client/src/assets/functions/toast.ts
@@ -2,28 +2,8 @@ import Vue from "vue";
 
 const toast = {
     send(_this: Vue, title: string, message: string, type: string, append = false) {
-        const element = _this.$createElement;
-
-        // Create the message
-        const vNodesMsg = element(
-            'p',
-            { class: ['text-break', 'mb-0'] },
-            [
-                element('strong'), `${message}`,
-            ]
-        );
-
-        // Create the title
-        const vNodesTitle = element(
-            'p',
-            { class: ['text-break', 'mr-2'] },
-            [
-                element('strong', { class: 'mr-2' }, title),
-            ],
-        );
-
-        _this.$bvToast.toast([vNodesMsg], {
-            title: [vNodesTitle],
+        _this.$bvToast.toast(title, {
+            title: message,
             toaster: "b-toaster-bottom-right",
             variant: type,
             solid: true,
