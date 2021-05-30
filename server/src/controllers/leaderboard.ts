ddf3b086efbbdd54d49cdb9d6d579858c473f913
kiran <kiran.singh@student.uhasselt.be>
2021-05-30 14:08:06 +0200

    leaderboard data

--- server/src/controllers/leaderboard.ts
+++ server/src/controllers/leaderboard.ts
@@ -1,8 +1,9 @@
 /**
  * @author Kiran Singh
  */
+import { responseSolve } from '@/routes/challenges';
 import { Request, Response } from 'express';
-import DB, { Account, Solve, Sponsor } from '../database';
+import DB, { Account, Solve, Sponsor, Team } from '../database';
 import socketIO from './socket';
 
 declare interface raw_data {
@@ -14,7 +15,7 @@ declare interface raw_data {
 declare interface data_send {
     uuid: string;
     name: string;
-    scores: { date: Date; score: number }[];
+    scores: { time: string; score: number }[];
 }
 
 /**
@@ -25,11 +26,9 @@ export class LeaderBoardController {
     teams: data_send[] = [];
 
     constructor() {
-        this.generatedData = this.generatedData.bind(this);
         this.updateLeaderboard = this.updateLeaderboard.bind(this);
         this.update = this.update.bind(this);
         this.getAllData = this.getAllData.bind(this);
-        this.generatedData();
 
     }
 
@@ -81,65 +80,26 @@ export class LeaderBoardController {
     * @returns {Team[]} This is the response
     */
     public getAllData(req: Request, res: Response) {
-        //TODO: get real data from @lander
-        // let rawData:raw_data = getRawData();
-        //console.log(this.teams);
-        res.json(this.teams);
-    }
 
-    /**
-     * Create fake data for the leaderboard
-     * @returns {Team[]}
-     */
-    public generatedData() {
-        var team1_points = 0;
-        var team2_points = 0;
-        var team3_points = 0;
-
-        this.teams.push({
-            uuid: "1",
-            name: "test1",
-            scores: [],
-        });
-        this.teams.push({
-            uuid: "2",
-            name: "test2",
-            scores: [],
-        });
-        this.teams.push({
-            uuid: "3",
-            name: "test3",
-            scores: [],
-        });
-
-        for (var day = 0; day < 1; day++) {
-            for (var hour = 0; hour < 5; hour++) {
-                for (var minute = 0; minute < 60; minute += 30) {
-                    team1_points += (minute + 5) * 2;
-                    team2_points += (minute + 3) * 1.5;
-                    team3_points += (minute + 3) * 3;
-
-                    var newDate1 = new Date(2020, 0, day, hour, Math.floor(Math.random() * ((minute + 4) - minute + 1) + minute), 0);
-                    var newDate2 = new Date(2020, 0, day, hour, Math.floor(Math.random() * ((minute + 4) - minute + 1) + minute), 0);
-                    var newDate3 = new Date(2020, 0, day, hour, Math.floor(Math.random() * ((minute + 4) - minute + 1) + minute), 0);
-
-                    this.teams[0].scores.push({
-                        date: newDate1,
-                        score: team1_points,
-                    });
-                    this.teams[1].scores.push({
-                        date: newDate2,
-                        score: team2_points,
-                    });
-                    this.teams[2].scores.push({
-                        date: newDate3,
-                        score: team3_points,
-                    });
-                }
-            }
+        const solvePoints = (team: Team, solve: Solve): number => {
+            return team.usedHints.filter(h => h.challenge.id == solve.challenge.id).reduce((acc, cur) => Math.max(acc - cur.hint.cost, 0), solve.challenge.points);
         }
-        return this.teams;
+
+        DB.repo(Team).find({ relations: ['solves', 'solves.challenge', 'usedHints', 'usedHints.challenge'] })
+            .then((teams) => {
+                let data: data_send[] = teams.map(team => ({
+                    uuid: team.id,
+                    name: team.name,
+                    scores: team.solves.map(solve => ({
+                        time: solve.time,
+                        score: solvePoints(team, solve)
+                    }))
+                }));
+                res.json(data);
+            })
+
     }
+
 
     /**
     * Route to get data sponsors dat such as link, img path, etc.
